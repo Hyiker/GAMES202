8784e7154d01cbc6d54b0d928d3b9cc52d4e9e10
hyiker <hyiker@bupt.edu.cn>
2021-11-26 17:15:53 +0800

    assignment1-1

---
+++ assignment1/src/lights/DirectionalLight.js
@@ -0,0 +1,42 @@
+class DirectionalLight {
+
+    constructor(lightIntensity, lightColor, lightPos, focalPoint, lightUp, hasShadowMap, gl) {
+        this.mesh = Mesh.cube(setTransform(0, 0, 0, 0.2, 0.2, 0.2, 0));
+        this.mat = new EmissiveMaterial(lightIntensity, lightColor);
+        this.lightPos = lightPos;
+        this.focalPoint = focalPoint;
+        this.lightUp = lightUp
+        console.log("lightUp", lightUp);
+
+        this.hasShadowMap = hasShadowMap;
+        console.log("lightUp", this.lightUp);
+        this.fbo = new FBO(gl);
+        if (!this.fbo) {
+            console.log("无法设置帧缓冲区对象");
+            return;
+        }
+    }
+
+    CalcLightMVP(translate, scale) {
+        let lightMVP = mat4.create();
+        let modelMatrix = mat4.create();
+        let viewMatrix = mat4.create();
+        let projectionMatrix = mat4.create();
+        
+        // Model transform
+        mat4.translate(modelMatrix, modelMatrix, translate);
+        mat4.scale(modelMatrix, modelMatrix, scale);
+
+        // View transform
+        mat4.lookAt(viewMatrix, this.lightPos, this.focalPoint, this.lightUp);
+    
+        // Projection transform
+        let lrtb = 90;
+        mat4.ortho(projectionMatrix, -lrtb, lrtb, -lrtb, lrtb, 1e-4, 400);
+
+        mat4.multiply(lightMVP, projectionMatrix, viewMatrix);
+        mat4.multiply(lightMVP, lightMVP, modelMatrix);
+
+        return lightMVP;
+    }
+}
